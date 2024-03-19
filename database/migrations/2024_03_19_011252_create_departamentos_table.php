bcb2d7a73d7a3edd62c33a81d28d86e2277aeb5f
Gunther37 <jmelvin1994@gmail.com>
2024-03-18 21:02:19 -0600

    Creacion de migraciones

--- database/migrations/2024_03_19_011252_create_departamentos_table.php
+++ database/migrations/2024_03_19_011252_create_departamentos_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('departamentos', function (Blueprint $table) {
+            $table->id();
+            $table->string('departamento', 60); // Campo 'departamento' de tipo varchar con longitud 60
+            $table->timestamps();
         });
     }
 
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('departamentos');
     }
 };
