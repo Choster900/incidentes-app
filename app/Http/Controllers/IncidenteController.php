6514dc64644e405dbef63b5a43a6b2d7ed61efcf
Segio Lopez <tec2.unifsoporte@isri.gob.sv>
2024-03-20 09:56:04 -0600

    Agregando funcionalidad al agregar

--- app/Http/Controllers/IncidenteController.php
+++ app/Http/Controllers/IncidenteController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Imagen;
+use App\Models\Incidente;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class IncidenteController extends Controller
 {
@@ -28,4 +32,71 @@ class IncidenteController extends Controller
     /* public function dash(){
         return view('admin.index');
     } */
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        try {
+            $descripcion = $request->descripcion;
+
+
+            $incidente = new Incidente();
+            $incidente->descripcion = $descripcion;
+            $incidente->usuario_id = 1; // Dato quedamo por el momento
+            $incidente->tecnico_id = 1; // Dato quedamo por el momento
+            $incidente->estado = 1;
+            $incidente->fecha_registro = Carbon::now();
+
+
+
+            $imageUrls = []; // Inicializamos un array para almacenar las URL de las imágenes
+
+
+                // Iteramos sobre cada archivo recibido
+                foreach ( $request->file("imagenes") as $file ) {
+                    // Obtener el nombre original del archivo
+                    $name = $file->getClientOriginalName();
+                    // Generar un nombre único para el archivo
+                    $currentTimestamp = time();
+                    $nameFile = date('Y-m-d His', $currentTimestamp) . '.' . $file->getClientOriginalExtension();
+                    // Almacenar el archivo en el directorio 'imagenes' dentro del disco 'public'
+                    $file->storeAs('imagenes', $nameFile, 'public');
+                    // Generar la URL completa para el archivo almacenado
+                    $imageUrl = asset('storage/imagenes/' . $nameFile); // Asegúrate de que la ruta sea correcta según la configuración de tu sistema de archivos
+                    // Agregar la URL al array de URLs de imágenes
+                    $imageUrls[] = $imageUrl;
+                }
+
+
+            if ($incidente->save() > 0) {
+                $incidenteId = $incidente->id; // Aquí obtenemos el ID insertado
+
+                // Iteramos sobre cada URL de imagen
+                foreach ( $imageUrls as $imageUrl ) {
+                    $imagen = new Imagen();
+                    $imagen->url = $imageUrl;
+                    $imagen->incidente_id = $incidenteId;
+                    $imagen->save();
+                }
+
+                return response()->json([
+                    "status"    => 'Created',
+                    "data"      => $incidenteId,
+                    "PRUEBA" => $imageUrls,
+                    "message"   => 'Incidente registrado'
+                ], 201);
+            } else {
+                return response()->json([
+                    "status"  => 'fail',
+                    "data"    => null,
+                    "message" => "Error al intentar guardar el departamento"
+                ], 409);
+            }
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
+    }
 }
--- resources/js/components/IncidenteComponent.vue
+++ resources/js/components/IncidenteComponent.vue
@@ -46,22 +46,24 @@
                     <div class="field col-md-6 col-sm-6">
                         <!-- Aquí estamos especificando que ocupe la mitad del ancho en dispositivos medianos y pequeños -->
                         <label for="marca">Tipo incidente</label>
-                        <Dropdown :options="[{ name: 'Australia', code: 'AU' },
-                        ]" optionLabel="nombre" optionValue="id" placeholder="Seleccione tipo incidente" />
+                        <Dropdown :options="citizen" optionLabel="label" optionValue="value"
+                            placeholder="Seleccione tipo incidente" />
                         <!--  <small class="p-error" v-if="submitted && !producto.marca_id">Marca es requerido.</small> -->
                     </div>
 
                 </div>
                 <div class="field">
                     <label for="descripcion">Descripcion</label>
-                    <Textarea id="descripcion" required="true" rows="2" cols="20" />
+                    <Textarea id="descripcion" required="true" rows="2" cols="20" v-model="incidente.descripcion" />
                     <!-- <small class="p-error" v-if="submitted && !producto.descripcion">Descripcion es requerido.</small> -->
                 </div>
                 <div class="formgrid grid row">
                     <div class="field col">
                         <label for="imagenes">Imágenes</label>
+                        {{ incidente.imagenes }}
                         <div class="custom-file">
-                            <input type="file" class="custom-file-input" id="imagenes" multiple accept="image/*">
+                            <input type="file" class="custom-file-input" id="imagenes" multiple accept="image/*"
+                                @change="getImages">
                             <label class="custom-file-label" for="imagenes">Seleccionar archivos...</label>
                         </div>
                     </div>
@@ -69,8 +71,8 @@
 
             </div>
             <template #footer>
-                <Button label="Cancelar" icon="pi pi-times" text @click="hideDialog" />
-                <Button label="Guardar" icon="pi pi-check" text @click="saveOrUpdate" />
+                <Button label="Cancelar" icon="pi pi-times" text @click="incidenteDialog = false" />
+                <Button label="Guardar" icon="pi pi-check" text @click="createIncidente" />
             </template>
         </Dialog>
     </div>
@@ -86,19 +88,62 @@ export default {
         return {
             incidentes: [],
             incidente: {
-                'id': null,
-                'descripcion': null,
-                'usuario_id': null,
-                'tecnico_id': null,
-                'estado': null,
-                'fecha_registro': null,
-                'created_at': null,
-                'updated_at': null,
+                id: null,
+                descripcion: null,
+                usuario_id: null,
+                tecnico_id: null,
+                estado: null,
+                fecha_registro: null,
+                created_at: null,
+                updated_at: null,
+                imagenes: [],
 
             },
-            incidenteDialog: ref(false)
+            incidenteDialog: ref(false),
+            citizen: [
+                { value: 'New ', label: 'Tipo Incidente 1' },
+                { value: 'Rome', label: 'Tipo Incidente 2' },
+                { value: 'London', label: 'Tipo Incidente 3' },
+                { value: 'Istanbul', label: 'Tipo Incidente 4' },
+                { value: 'Paris', label: 'Tipo Incidente 5' }
+            ]
         }
     },
+    methods: {
+        getImages(event) {
+            const files = Array.from(event.target.files); // Convertimos el objeto FileList a un array
+            files.forEach(file => {
+                const { name } = file; // Utilizamos destructuring para obtener el nombre del archivo
+                this.incidente.imagenes.push({ fileName: name, file }); // Usamos la sintaxis simplificada de objetos
+            });
+        },
+
+        async createIncidente() {
+            try {
+                const formData = new FormData();
+                formData.append("descripcion", this.incidente.descripcion);
+                // Agregar cada archivo al FormData
+                this.incidente.imagenes.forEach(image => {
+                    formData.append("imagenes[]", image.file);
+                });
+                const resp = await axios.post('/api/incidente', formData, {
+                    headers: {
+                        "Content-Type": "multipart/form-data",
+                    },
+                });
+
+                console.log('Respuesta del servidor:', resp.data);
+
+                // Aquí puedes manejar la respuesta del servidor como desees
+
+            } catch (error) {
+                console.error('Error al subir el archivo:', error);
+
+                // Aquí puedes manejar el error como desees
+            }
+        }
+    },
+
 
 
 
